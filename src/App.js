e10d516a20b94e074ca8448cffc4e28848b91f8d
Hernan <hernanlogran@hotmail.com>
2020-01-25 19:43:57 -0300

    Add connection to api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import Cell from './components/Cell'
+import React, { useState, useEffect } from "react";
+import {API_URL} from './constants';
+import Board from './components/Board'
 import './App.css';
 
 function App() {
+  const [game, setGame] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${API_URL}/games`, {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          "rows": 10,
+          "cols": 10,
+          "bombs": 20
+        })
+      });
+
+      const game = await response.json();
+      setGame(game);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <Cell/>
+      <h1>Minesweeper game</h1>
+      <Board game={game}/>
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    API_URL: 'https://hernan-logran-minesweeper-api.herokuapp.com'
+};
