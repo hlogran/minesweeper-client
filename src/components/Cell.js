370a865583489ccadc703e1a26a378b12ae14518
Hernan <hernanlogran@hotmail.com>
2020-01-25 19:21:51 -0300

    Create component Cell

---
+++ src/components/Cell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Cell(props){
+    return (
+        <div style={{
+            width: '30px',
+            height: '30px',
+            backgroundColor: 'darkgray',
+            border: 'solid thin'
+        }}>
+
+        </div>
+    )
+}
